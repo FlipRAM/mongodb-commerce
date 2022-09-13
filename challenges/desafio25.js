5c1640c165ceb2e1e108bbe9db968460d79078e0
Felipe Rangel <feliperangel277@gmail.com>
2022-09-13 14:09:59 -0300

    Fix lint errors

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,2 +1,4 @@
-db.produtos.updateMany({ 'valoresNutricionais.2.percentual': { $gte: 40 } }, { $push: { tags: 'muito sódio' } });
+db.produtos.updateMany({
+  "valoresNutricionais.2.percentual": { $gte: 40 } }, { $push: { tags: "muito sódio" },
+});
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
